4b6ad6ecd6670fd43d52aed4618167096a7d1f8c
Ryan Sweeney <ryan.sweeney@pensiamoinc.com>
2019-10-05 13:15:15 -0400

    Arrays-1 solution.

---
+++ Hackerrank.Tests/Arrays/Challenge1Tests.cs
@@ -0,0 +1,42 @@
+﻿using Hackerrank.Arrays;
+using NUnit.Framework;
+
+namespace Hackerrank.Tests.Arrays
+{
+    [TestFixture]
+    public class Challenge1Tests
+    {
+
+        private static readonly int[][] ChallengeInput = {
+            new[] {1, 1, 1, 0, 0, 0},
+            new[] {0, 1, 0, 0, 0, 0},
+            new[] {1, 1, 1, 0, 0, 0},
+            new[] {0, 0, 2, 4, 4, 0},
+            new[] {0, 0, 0, 2, 0, 0},
+            new[] {0, 0, 1, 2, 4, 0}
+        };
+
+        private static readonly int[][] TestCase3Input =
+        {
+            new[] {-1, -1, 0, -9, -2, -2},
+            new[] {-2, -1, -6, -8, -2, -5,},
+            new[] {-1, -1, -1, -2, -3, -4},
+            new[] {-1, -9, -2, -4, -4, -5},
+            new[] {-7, -3, -3, -2, -9, -9},
+            new[] {-1, -3, -1, -2, -4, -5}
+        };
+        
+        [Test(Description = "Sum is calculated correctly.")]
+        public void CalculateSum()
+        {
+            Assert.AreEqual(19, Challenge1.HourglassSum(ChallengeInput));
+        }
+
+        [Test(Description = "Calcluates test case 3 correctly.")]
+        public void TestCase3()
+        {
+            Assert.AreEqual(-6, Challenge1.HourglassSum(TestCase3Input));
+        }
+        
+    }
+}
---
+++ Hackerrank/Arrays/Challenge1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hackerrank.Arrays
+{
+    //https://www.hackerrank.com/challenges/2d-array/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+    public static class Challenge1
+    {
+
+        public static int HourglassSum(int[][] arr)
+        {
+            var maxHourglass = int.MinValue;
+
+            for (var i = 0; i < 4; i++)
+            {
+                for (var j = 0; j < 4; j++)
+                {
+                    var sum = CalculateHourglass(i, j, arr);
+                    if (sum > maxHourglass)
+                    {
+                        maxHourglass = sum;
+                    }
+                }
+            }
+
+            return maxHourglass;
+        }
+
+        private static int CalculateHourglass(int x, int y, IReadOnlyList<int[]> arr)
+        {
+            var sum = 0;
+            for (var i = y; i <= y + 2; i++)
+            {
+                for (var j = x; j <= x + 2; j++)
+                {
+                    if (i != y + 1 || (j != x && j != x + 2))
+                    {
+                        sum += arr[i][j];
+                    }
+                }
+            }
+            
+            return sum;
+        }
+        
+    }
+}
